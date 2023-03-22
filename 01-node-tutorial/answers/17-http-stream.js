74d908ca05a2ee16af562365c4723a994ce69b28
cdpaiva <cdpaivaa@gmail.com>
2023-03-22 09:43:42 -0400

    Avoids ERR_INVALID_ARG_TYPE when error is emmited

--- 01-node-tutorial/answers/17-http-stream.js
+++ 01-node-tutorial/answers/17-http-stream.js
@@ -3,12 +3,13 @@ var fs = require("fs");
 
 http
   .createServer(function (req, res) {
+    //const fileStream = fs.createReadStream("non-existing-file.txt", "utf8");
     const fileStream = fs.createReadStream("./content/big.txt", "utf8");
     fileStream.on("open", () => {
       fileStream.pipe(res);
     });
     fileStream.on("error", (err) => {
-      res.end(err);
+      res.end(err.message);
     });
   })
   .listen(5000);
