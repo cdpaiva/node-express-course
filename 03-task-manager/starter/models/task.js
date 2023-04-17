e57eed438f4bc3cafc8ee7d566ed97e9725de9c7
cdpaiva <cdpaivaa@gmail.com>
2023-04-17 08:12:15 -0400

    answers for lesson 6

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -3,8 +3,16 @@ const mongoose = require("mongoose");
 // The schema is the structure of our models
 // Extra properties passed from our request will be ignored
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide a name"],
+    trim: true,
+    maxlength: [20, "Name cannot have more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
