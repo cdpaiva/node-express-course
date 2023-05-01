54ac7ab6fc13d6fc8ffbfc2af9f98d3835c15337
cdpaiva <cdpaivaa@gmail.com>
2023-05-01 15:29:41 -0400

    Code from following the video

---
+++ 05-JWT-Basics/starter/errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require("./custom-error.js");
+const BadRequest = require("./bad-request.js");
+const UnauthenticatedError = require("./unauthenticated.js");
+
+module.exports = { CustomAPIError, BadRequest, UnauthenticatedError };
